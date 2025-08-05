f05bef700874d23a75df911cab99ad1eba2fd395
Qora <lorismziray01@gmail.com>
2025-08-05 09:33:14 +0200

    update

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "[DB] Nordsüdbahn-V1";
+const BASE = "/Nordsuedbahn/";
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        
+        BASE + 'index.html'
+        
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.open(CACHE_NAME).then(cache =>
+    cache.match(event.request).then(resp => {
+      return resp || fetch(event.request).then(fresp => {
+        cache.put(event.request, fresp.clone());
+        return fresp;
+      });
+    })
+  ));
+});
